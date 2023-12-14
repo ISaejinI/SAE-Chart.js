5e39be9548c21571a3fcca62308e2b52a34be4d2
Lou-Anne <saejinmx@gmail.com>
2023-12-14 11:14:16 +0100

    Tab qui s'affiche

--- dist/index.ad096df2.js
+++ dist/index.ad096df2.js
@@ -583,12 +583,55 @@ const req = new XMLHttpRequest();
 let data = null;
 req.addEventListener("load", (evt)=>{
     data = JSON.parse(req.responseText);
+    data = data[2].data;
     console.log(data);
+    let tab1 = [
+        50,
+        42,
+        8
+    ];
+    //Pour rechercher le nom d'un truc
+    // const total = data.filter((test)=> test.solver_id == 17);
+    // console.log(total);
+    (async function() {
+        new (0, _autoDefault.default)(document.getElementById("graph1"), {
+            type: "doughnut",
+            data: {
+                labels: [
+                    "ACE",
+                    "Choco",
+                    "Picat",
+                    "BTD",
+                    "CoSoCo",
+                    "Fun-sCOP-cad",
+                    "Fun-sCOP-glue",
+                    "Mistral",
+                    "Sat4j-cp",
+                    "Sat4j-rs"
+                ],
+                datasets: [
+                    {
+                        label: "Pourcentage",
+                        data: tab1
+                    }
+                ]
+            },
+            options: {
+                responsive: true,
+                plugins: {
+                    legend: {
+                        position: "right"
+                    },
+                    title: {
+                        display: false
+                    }
+                }
+            }
+        });
+    })();
 });
 req.open("GET", "https://www.cril.univ-artois.fr/~lecoutre/teaching/jssae/code5/results.json");
-req.send(); // (async function() {
- //     const data = 
- // })
+req.send();
 
 },{"chart.js/auto":"d8NN9","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"d8NN9":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
